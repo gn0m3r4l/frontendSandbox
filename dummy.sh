8b1c020561056f47c79f26405ae976ba671059b8
gn0m3r4l <daniel.plagge@googlemail.com>
2025-11-19 14:45:43 +0100

    2 builds

--- dummy.sh
+++ dummy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
  echo "Erzeuge Dummy-Datei (5 MB)…"
-          dd if=/dev/zero of=dummy.bin bs=1M count=5
-          tar -czf test.tar.gz dummy.bin
-          rm dummy.bin
-          echo "Dummy-Datei test.tar.gz erzeugt."
+  dd if=/dev/zero of=dummy.bin bs=1M count=5
+  dd if=/dev/zero of=dummy2.bin bs=1M count=5
+
+  tar -czf test.tar.gz dummy.bin
+  tar -czf test2.tar.gz dummy2.bin
+  rm dummy.bin
+  rm dummy2.bin
+  echo "Dummy-Datei test.tar.gz erzeugt."
